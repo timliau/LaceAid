c9856d07310a17746f63d7bd16620ce30259f532
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2022-01-13 19:50:18 +0800

    added animation to splash screen

--- app/src/main/java/com/sp/laceaid/Log_in_Activity.java
+++ app/src/main/java/com/sp/laceaid/Log_in_Activity.java
@@ -3,12 +3,14 @@ package com.sp.laceaid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Log_in_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
         setContentView(R.layout.activity_log_in);
     }
 }
