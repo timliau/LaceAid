7775c929e8ccd05d686389d2327726a1eec106ad
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2022-02-04 21:31:26 +0800

    Nav header now can update name and email from db

--- app/src/main/java/com/sp/laceaid/MainActivity.java
+++ app/src/main/java/com/sp/laceaid/MainActivity.java
@@ -13,12 +13,21 @@ import androidx.navigation.ui.NavigationUI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sp.laceaid.login.screen.LoginOptionsActivity;
 import com.sp.laceaid.uiNavDrawer.AboutActivity;
 import com.sp.laceaid.uiNavDrawer.InfoActivity;
@@ -29,9 +38,13 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    private FirebaseUser user;
+    private DatabaseReference databaseReference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,14 +74,56 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
+        // grab data from firebase realtime db
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        // need to add the url as the default location is USA not SEA
+        databaseReference = FirebaseDatabase.getInstance("https://lace-aid-default-rtdb.asia-southeast1.firebasedatabase.app").getReference("Users");
+        userID = user.getUid();
+
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false); // hide title
 
         // navigation drawer menu
         navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            // Called when a drawer has settled in a completely closed state.
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                //Toast.makeText(MainActivity.this, "drawer closed", Toast.LENGTH_LONG).show();
+            }
+
+            // Called when a drawer has settled in a completely open state.
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //Toast.makeText(MainActivity.this, "drawer opened", Toast.LENGTH_LONG).show();
+                View headerView = navigationView.getHeaderView(0);
+                TextView nvName = headerView.findViewById(R.id.tvnh_name);
+                TextView nvEmail = headerView.findViewById(R.id.tvnh_email);
+
+                databaseReference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User userprofile = snapshot.getValue(User.class);
+
+                        if(userprofile != null) {
+                            String name = userprofile.getName();
+                            String email = userprofile.getEmail();
+                            nvName.setText(name);
+                            nvEmail.setText(email);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainActivity.this, "Cannot update profile", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        };
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
         navigationView.setNavigationItemSelectedListener(this);
     }
 
--- app/src/main/java/com/sp/laceaid/User.java
+++ app/src/main/java/com/sp/laceaid/User.java
@@ -1,13 +1,22 @@
 package com.sp.laceaid;
 
 public class User {
-    public String name, email;
+    // class to group data for storing in realtime database
+    private String name, email;
 
-    public User(){
+    private User(){
     }
 
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
